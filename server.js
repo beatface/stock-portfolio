5fe7330a4a28c5f07a891eee64699d472d871f22
beatface <emma.beaton@gmail.com>
2016-02-15 15:15:50 -0600

    inital file structure setup and server is listening

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+/*eslint-disable no-alert, no-console */
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('yup!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
